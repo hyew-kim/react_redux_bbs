516005ac6c4ffafaf2d48308d66bf4cafd134b64
hyunwk <ab8454@naver.com>
2022-06-04 21:32:05 +0900

    feat: skeleton code

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './Home'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+
+<>
+
+</>
+export default function Navbar() {
+  return (
+      <>
+        <div>Navbar</div>
+        <Home />
+        <SignIn />
+        <SignUp /> 
+      </>
+  )
+}
