fd51aba559c91166a66544c01b810c5282bf4018
hyunwk <ab8454@naver.com>
2022-06-11 20:00:49 +0900

    feature: Sign component submit button

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,20 +1,24 @@
 import "../Sign.css"
-export default function SignIn() {
+export default function SignUp() {
   return (
     <div className="login-container"> 
-      <div className="login-title">Sign In</div>
+      <div className="login-title">Sign Up</div>
       <form className="login-box">
+        <div className="user-name">
+          <label>USER NAME</label>
+          <input className="login-input" type="text" name="user-name" />
+        </div>
         <div className="email">
           <label>EMAIL</label>
           <input className="login-input" type="text" name="email" />
         </div>
         <div className="password">
           <label>PASSWORD</label>
-          <input className="login-input" type="password" name="password" />
+          <input className="login-input" type="text" name="password" />
         </div>
         <div className="submit">
           <button>
-            Sign In 
+            Sign Up 
           </button>
         </div>
       </form>
