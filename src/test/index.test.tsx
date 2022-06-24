9ee849d755d02688ad1b1cbca5b8cb40d8689ee6
Kim Hyewon <79592604+hyew-kim@users.noreply.github.com>
2022-06-24 20:09:16 +0900

    feat: test with jest 적용

---
+++ src/test/index.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "../App";
+import { store } from "../app/store";
+
+test("renders learn react link", () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  expect(getByText(/PLACE FOR LOGO/i)).toBeInTheDocument();
+});
